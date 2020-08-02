5411d56d431e48ef9d9659c3c6decd87e939f48a
Heber <heber.trj.urt@gmail.com>
2020-08-02 16:31:35 +0200

    This commit adds the get_batches function to feed the RNN

--- data_prep/encode_data.py
+++ data_prep/encode_data.py
@@ -3,7 +3,7 @@ def get_vocabulary(text):
         '''Creates the vocabulary needed to represent a corpus as numerc values
             Arguments
             ---------
-            text: Corpus you want represent as numerc values 
+            text: Corpus you want represent as numerc values
 
         '''
     chars = tuple(set(text))
@@ -38,3 +38,38 @@ def one_hot_encode(arr, n_labels):
     # Finally reshape it to get back to the original array
     one_hot = one_hot.reshape((*arr.shape, n_labels))
     return one_hot
+
+
+def get_batches(arr, batch_size, seq_length):
+    '''Create a generator that returns batches of size
+       batch_size * seq_length from arr.
+
+       Arguments
+       ---------
+       arr: Array you want to make batches from
+       batch_size: Batch size, the number of sequences per batch
+       seq_length: Number of encoded chars in a sequence
+    '''
+
+    ## Get the number of batches we can make
+    n_batches = int(len(arr)/(batch_size*seq_length))
+
+    ## Keep only enough characters to make full batches
+    arr = arr[:batch_size*seq_length *n_batches]
+
+    ## Reshape into batch_size rows
+    arr = arr.reshape((batch_size,-1))
+
+    ## Iterate over the batches using a window of size seq_length
+    for n in range(0, arr.shape[1], seq_length):
+        # The features
+        x = arr[:, n:n+seq_length]
+        # The targets, shifted by one
+        y = np.zeros_like(x)
+        try:
+            y[:, :-1] = x[:, 1:]
+            y[:,-1] = arr[:,n+seq_length]
+        except IndexError:
+            y[:, :-1] = x[:, 1:]
+            y[:,-1] = arr[:,0]
+        yield x, y
